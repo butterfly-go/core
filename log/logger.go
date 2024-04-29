54a83053994633ab04ebe2a392476aeb19bf36c7
orvice L <orvice@gmail.com>
2024-04-29 15:41:49 +0800

    add with logger

--- log/logger.go
+++ log/logger.go
@@ -10,3 +10,7 @@ type loggerContextKey struct{}
 func FromContext(context.Context) *slog.Logger {
 	return slog.Default()
 }
+
+func WithLogger(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerContextKey{}, logger)
+}
