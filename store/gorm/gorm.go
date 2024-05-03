00e9427dd0bf2b31d0a453e99f66e8bdfd43be25
orvice <orvice@gmail.com>
2024-05-04 01:54:26 +0800

    init gorm support (#8)
    * init gorm support
    
    * fix lint
    
    * add session,tx alias

---
+++ store/gorm/gorm.go
@@ -0,0 +1,27 @@
+package gorm
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/plugin/opentelemetry/tracing"
+)
+
+type DB = gorm.DB
+
+type Session = gorm.Session
+
+type Tx = gorm.Tx
+
+// NewDB
+// MySQL only for now
+func NewDB(dsn string) (*DB, error) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Use(tracing.NewPlugin()); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
