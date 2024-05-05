35fd066e86e1055931da2cdf80586a839d2f1d31
orvice L <orvice@gmail.com>
2024-05-05 15:34:32 +0800

    config init

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,24 @@ package config
 
 import "context"
 
+var (
+	config Config
+)
+
 type Config interface {
 	Get(ctx context.Context, key string) ([]byte, error)
+}
+
+func Init() error {
+	c, err := NewConsulConfig()
+	if err != nil {
+		return err
+	}
+	// set config
+	config = c
+	return nil
+}
+
+func GetConfig() Config {
+	return config
 }
