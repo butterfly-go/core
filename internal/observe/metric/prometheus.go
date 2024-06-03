02da2f7dfdb822e11732c0c5b8425e1ff0e5c147
orvice L <orvice@gmail.com>
2024-06-04 00:17:33 +0800

    err check

--- internal/observe/metric/prometheus.go
+++ internal/observe/metric/prometheus.go
@@ -1,6 +1,8 @@
 package metric
 
 import (
+	"log/slog"
+
 	"butterfly.orx.me/core/internal/arg"
 	"butterfly.orx.me/core/internal/runtime"
 	"github.com/prometheus/client_golang/prometheus/push"
@@ -9,5 +11,8 @@ import (
 func PrometheusPush() {
 	addr := arg.String("prometheus.push.endpoint")
 	pusher := push.New(addr, runtime.Service()).Gatherer(registry)
-	pusher.Push()
+	err := pusher.Push()
+	if err != nil {
+		slog.Error("PrometheusPush failed", "error", err.Error())
+	}
 }
