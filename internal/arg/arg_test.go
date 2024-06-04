75f52edc2aa28c84091b6f4fcecfbd674868cf48
orvice L <orvice@gmail.com>
2024-06-05 00:50:49 +0800

    update arg parse

--- internal/arg/arg_test.go
+++ internal/arg/arg_test.go
@@ -9,5 +9,7 @@ import (
 
 func TestArgString(t *testing.T) {
 	os.Setenv("BUTTERFLY_TRACING_ENDPOINT", "otel")
+	os.Setenv("BUTTERFLY_CONFIG_CONSUL_ADDRESS", "consul:8500")
 	assert.Equal(t, String("tracing-endpoint"), "otel")
+	assert.Equal(t, String("config.consul.address"), "consul:8500")
 }
