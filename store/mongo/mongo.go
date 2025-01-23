835687da367b27ac5f5ae84dba1593fa1db24c1b
orvice L <orvice@gmail.com>
2025-01-23 21:45:06 +0800

    upgrade mongo to v2

--- store/mongo/mongo.go
+++ store/mongo/mongo.go
@@ -2,7 +2,7 @@ package mongo
 
 import (
 	"butterfly.orx.me/core/internal/store"
-	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func GetClient(k string) *mongo.Client {
